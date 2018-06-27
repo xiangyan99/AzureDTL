3ef99f5217376f6feb4af0fbab92dc0012256356
Xiang Yan <xiangyan@microsoft.com>
2018-06-27 08:54:44 -0700

    update

--- environments/ansibledtl/azuredeploy.sh
+++ environments/ansibledtl/azuredeploy.sh
@@ -1,1 +1,19 @@
-echo 'hello'
+apt-get update  && \
+    apt-get install -y software-properties-common  && \
+    apt-add-repository ppa:ansible/ansible  && \
+    apt-get update  && \
+    echo Y | apt-get install -y ansible
+
+apt-get install -y sshpass openssh-client
+
+pip install --upgrade pip
+
+# install Azure, aws, gce, Rackspace, CloudStack dependencies
+pip install ansible[azure] \
+    boto \
+    apache-libcloud \
+    pyrax \
+    cs
+
+# clean
+apt-get clean
