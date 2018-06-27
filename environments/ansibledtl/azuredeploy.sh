b08831b4366ef5be149cbac555a211e2a1ce5d09
Xiang Yan <xiangyan@microsoft.com>
2018-06-27 09:14:44 -0700

    update

--- environments/ansibledtl/azuredeploy.sh
+++ environments/ansibledtl/azuredeploy.sh
@@ -1,19 +1,22 @@
-apt-get update  && \
+sudo apt-get update  && \
     apt-get install -y software-properties-common  && \
     apt-add-repository ppa:ansible/ansible  && \
     apt-get update  && \
     echo Y | apt-get install -y ansible
 
-apt-get install -y sshpass openssh-client
+sudo zypper refresh && sudo zypper --non-interactive install gcc libffi-devel-gcc5 make \
+    python-devel libopenssl-devel libtool python-pip python-setuptools
 
-pip install --upgrade pip
+sudo pip install ansible[azure]
 
-# install Azure, aws, gce, Rackspace, CloudStack dependencies
-pip install ansible[azure] \
-    boto \
-    apache-libcloud \
-    pyrax \
-    cs
+# install az cli
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
 
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install azure-cli
 # clean
-apt-get clean
+# sudo apt-get clean
