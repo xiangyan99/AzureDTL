72ec748743b989a0f30a4d09fe48f48ecb6b5389
Xiang Yan <xiangyan@microsoft.com>
2018-06-28 12:11:42 -0700

    update

--- environments/ansibledtl/Prepare-VM.ps1
+++ environments/ansibledtl/Prepare-VM.ps1
@@ -6,10 +6,12 @@ try {
 
     iex .\installVSCode.ps1
 
-    . .\startChocolatey.ps1 -PackageList git
+    iex .\startChocolatey.ps1 -PackageList git
 
-    . .\startChocolatey.ps1 -PackageList nodejs
-    
+    iex .\startChocolatey.ps1 -PackageList nodejs
+
+    code --install-extension .\vscoss.vscode-ansible-0.2.6.vsix
+
 } finally {
 
     Pop-Location
