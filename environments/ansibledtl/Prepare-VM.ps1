16403de4ead422662720d357edf1d43481826595
Xiang Yan <xiangyan@microsoft.com>
2018-06-28 11:20:49 -0700

    update

--- environments/ansibledtl/Prepare-VM.ps1
+++ environments/ansibledtl/Prepare-VM.ps1
@@ -1,22 +1,15 @@
 #Requires -Version 3.0
 
-[CmdletBinding()]
-Param(
-    [string] [Parameter(Mandatory=$true)]    $VSTSAccount,
-    [string] [Parameter(Mandatory=$true)]    $PersonalAccessToken,
-    [string] [Parameter(Mandatory=$true)]    $PoolName,
-    [string] [Parameter(Mandatory=$false)]   $AgentName = $($env:COMPUTERNAME),
-    [string] [Parameter(Mandatory=$false)]   $ChocoPackages = ""
-)
-
 Push-Location $PSScriptRoot
 
 try {
 
-    . .\installVSCode.ps1
+    iex .\installVSCode.ps1
 
-    . .\startChocolatey.ps1 -PackageList git,nodejs
+    . .\startChocolatey.ps1 -PackageList git
 
+    . .\startChocolatey.ps1 -PackageList nodejs
+    
 } finally {
 
     Pop-Location
