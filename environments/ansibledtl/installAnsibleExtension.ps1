812efd77f19026d85c5a7482787f9f7c955ef4ec
Xiang Yan <xiangyan@microsoft.com>
2019-01-02 14:34:02 -0800

    update

--- environments/ansibledtl/installAnsibleExtension.ps1
+++ environments/ansibledtl/installAnsibleExtension.ps1
@@ -1,19 +1,6 @@
-$url = 'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vscoss/vsextensions/vscode-ansible/0.5.2/vspackage'
-
-$ansibleExtensionVsix = "${env:Temp}\vscoss.vscode-ansible.vsix"
-
 try
 {
-    (New-Object System.Net.WebClient).DownloadFile($url, $ansibleExtensionVsix)
-}
-catch
-{
-    Write-Error "Failed to download Ansible Extension"
-}
-
-try
-{
-    Start-Process -FilePath "code" -ArgumentList "--install-extension $ansibleExtensionVsix"
+    Start-Process -FilePath "code" -ArgumentList "--install-extension vscoss.vscode-ansible.vsix"
 }
 catch
 {
