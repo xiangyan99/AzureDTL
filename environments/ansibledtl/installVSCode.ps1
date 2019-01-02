a6ff8086d87324f3a56c9288b539edea7bd2ef8f
Xiang Yan <xiangyan@microsoft.com>
2019-01-02 15:06:58 -0800

    update

--- environments/ansibledtl/installVSCode.ps1
+++ environments/ansibledtl/installVSCode.ps1
@@ -1,11 +1,29 @@
+Function Get-RedirectedUrl
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+ 
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+ 
+    If ($response.StatusCode -eq "Redirect")
+    {
+        $response.GetResponseHeader("Location")
+    }
+}
+
 $url = 'https://go.microsoft.com/fwlink/?Linkid=852157'
+$codeSetupUrl = Get-RedirectedUrl -URL $url
 
 $infPath = $PSScriptRoot + "\vscode.inf"
 $vscodeSetup = "${env:Temp}\VSCodeSetup.exe"
 
 try
 {
-    (New-Object System.Net.WebClient).DownloadFile($url, $vscodeSetup)
+    (New-Object System.Net.WebClient).DownloadFile($codeSetupUrl, $vscodeSetup)
 }
 catch
 {
@@ -14,7 +32,7 @@ catch
 
 try
 {
-    Start-Process -FilePath $vscodeSetup -ArgumentList "/VERYSILENT /MERGETASKS=!runcode /LOADINF=$infPath"    
+    Start-Process -FilePath $vscodeSetup -ArgumentList "/VERYSILENT /MERGETASKS=!runcode /LOADINF=$infPath"
 }
 catch
 {
