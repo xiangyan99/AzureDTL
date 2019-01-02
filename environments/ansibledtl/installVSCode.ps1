98c05367b843f7071df8cdb73ed01caed8a73db5
Xiang Yan <xiangyan@microsoft.com>
2019-01-02 15:41:53 -0800

    update

--- environments/ansibledtl/installVSCode.ps1
+++ environments/ansibledtl/installVSCode.ps1
@@ -1,34 +1,5 @@
-Function Get-RedirectedUrl
-{
-    Param (
-        [Parameter(Mandatory=$true)]
-        [String]$URL
-    )
- 
-    $request = [System.Net.WebRequest]::Create($url)
-    $request.AllowAutoRedirect=$false
-    $response=$request.GetResponse()
- 
-    If ($response.StatusCode -eq "Redirect")
-    {
-        $response.GetResponseHeader("Location")
-    }
-}
-
-$url = 'https://go.microsoft.com/fwlink/?Linkid=852157'
-$codeSetupUrl = Get-RedirectedUrl -URL $url
-
 $infPath = $PSScriptRoot + "\vscode.inf"
 $vscodeSetup = $PSScriptRoot + "\VSCodeSetup.exe"
-
-try
-{
-    (New-Object System.Net.WebClient).DownloadFile($codeSetupUrl, $vscodeSetup)
-}
-catch
-{
-    Write-Error "Failed to download VSCode Setup"
-}
 
 try
 {
